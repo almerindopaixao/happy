719f2fbb534eb5147747bec624d600000fab1963
almerindopaixao <almerindo_paixao@live.com>
2020-10-12 11:54:11 -0300

    :iphone: Update Langing Page

---
+++ web/src/pages/Landing/index.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import { FiArrowRight } from 'react-icons/fi';
+
+import './Landing.css';
+
+import logoImg from '../../assets/images/logo.svg';
+
+export default function Landing(): JSX.Element {
+  return (
+    <div id="page-landing">
+      <div className="content-wrapper">
+        <img src={logoImg} alt="Logo Happy" />
+
+        <main>
+          <h1>Leve felicidade para o mundo</h1>
+          <p>Visite orfanatos e mude o dia de muitas crianças.</p>
+        </main>
+
+        <div className="location">
+          <strong>Bahia</strong>
+          <span>Entre-Rios</span>
+        </div>
+
+        <Link to="/app" className="enter-app">
+          <FiArrowRight size={26} color="rgba(0, 0, 0, 0.6)" />
+        </Link>
+      </div>
+    </div>
+  );
+}
---
+++ web/src/pages/OrphanagesMap/index.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function OrphanagesMap(): JSX.Element {
+  return <h1>Hello word</h1>;
+}
