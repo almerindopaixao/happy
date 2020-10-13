ef58001f767714ca455778d323cb0f20ff2aba31
almerindopaixao <almerindo_paixao@live.com>
2020-10-13 19:19:52 -0300

    Update multer filter

--- server/src/config/uploads.ts
+++ server/src/config/uploads.ts
@@ -3,13 +3,31 @@ import { resolve, extname } from 'path';
 
 const rand = () => Math.floor(Math.random() * 10000 + 10000);
 
-export default {
-  storage: multer.diskStorage({
-    destination: (req, file, cb) => {
-      cb(null, resolve(__dirname, '..', '..', 'uploads', 'images'));
-    },
-    filename: (req, file, cb) => {
-      cb(null, `${Date.now()}_${rand()}${extname(file.originalname)}`);
-    },
-  }),
-};
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, resolve(__dirname, '..', '..', 'uploads', 'images'));
+  },
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}_${rand()}${extname(file.originalname)}`);
+  },
+});
+
+export default multer({
+  storage: storage,
+  fileFilter: (req, file, cb) => {
+    if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+      return cb(
+        new multer.MulterError(
+          'LIMIT_UNEXPECTED_FILE',
+          'Os arquivos enviados precisam ser do formato png ou jpg',
+        ),
+      );
+    }
+
+    return cb(null, true);
+  },
+
+  limits: {
+    fileSize: 1024 * 1024,
+  },
+});
--- server/src/middlewares/ImagesMiddleware.ts
+++ server/src/middlewares/ImagesMiddleware.ts
@@ -1,7 +1,23 @@
-import multer from 'multer';
-
+import { Request, Response, NextFunction, IRouter } from 'express';
 import multerConfig from '../config/uploads';
 
-const upload = multer(multerConfig).array('images');
+const upload = multerConfig.array('images');
 
-export default upload;
+function imagesMiddleware(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): IRouter {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  return upload(req, res, function (err: any) {
+    if (err) {
+      return res.status(400).json({
+        errors: [err.field],
+      });
+    }
+
+    next();
+  });
+}
+
+export default imagesMiddleware;
