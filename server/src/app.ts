4e8b1e01744eaa44ac467b555f9c92b06ae550ca
almerindopaixao <almerindo_paixao@live.com>
2020-10-13 11:45:35 -0300

    Create: Migrations e Update for database postgresql

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,8 +1,17 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import 'reflect-metadata';
 import 'dotenv/config';
+
+import './database/connection';
 
 class App {
   public app = express();
+
+  middlewares() {
+    this.app.use(bodyParser.json());
+    this.app.use(bodyParser.urlencoded({ extended: true }));
+  }
 }
 
 export default new App().app;
